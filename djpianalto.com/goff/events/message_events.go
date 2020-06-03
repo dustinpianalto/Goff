65fc3a5a6f25b9bf0900042ddf43406c819f6bfe
Dustin Pianalto <dustin@djpianalto.com>
2020-06-02 22:54:47 -0800

    Finish message update event

--- djpianalto.com/goff/events/message_events.go
+++ djpianalto.com/goff/events/message_events.go
@@ -3,10 +3,32 @@ package events
 import (
 	"fmt"
 
+	"djpianalto.com/goff/djpianalto.com/goff/utils"
 	"github.com/bwmarrin/discordgo"
 )
 
 func OnMessageUpdate(session *discordgo.Session, m *discordgo.MessageUpdate) {
-	fmt.Println(m.Content)
-	fmt.Println(m.BeforeUpdate.Content)
+	if m.Author.Bot {
+		return
+	}
+	var channelID string
+	row := utils.Database.QueryRow("SELECT logging_channel FROM guilds where id=$1", m.GuildID)
+	err := row.Scan(&channelID)
+	if err != nil || channelID == "" {
+		return
+	}
+	channel, err := session.State.Channel(m.ChannelID)
+	if err != nil {
+		return
+	}
+	embed := &discordgo.MessageEmbed{
+		Title:       fmt.Sprintf("Edited Message: %v in %v", m.ID, channel.Mention()),
+		Description: fmt.Sprintf("**Before:** %v\n**After:** %v", m.BeforeUpdate.Content, m.Content),
+		Color:       session.State.UserColor(m.Author.ID, channelID),
+		Footer: &discordgo.MessageEmbedFooter{
+			Text:    fmt.Sprintf("Author: %v", m.Author.String()),
+			IconURL: m.Author.AvatarURL(""),
+		},
+	}
+	session.ChannelMessageSendEmbed(channelID, embed)
 }
