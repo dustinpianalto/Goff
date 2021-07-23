02b5842107770cd329a7ee8643dc0b02df36cd50
Dustin Pianalto <dustin@djpianalto.com>
2021-07-23 12:54:23 -0800

    Add milliseconds to timestamp

--- internal/discord_utils/snowflake.go
+++ internal/discord_utils/snowflake.go
@@ -19,7 +19,10 @@ func ParseSnowflake(s int64) Snowflake {
 		PROCESS_ID_MASK = 0x1F000
 		INCREMENT_MASK  = 0xFFF
 	)
-	creationTime := time.Unix(((s>>TIME_BITS_LOC)+DISCORD_EPOCH)/1000.0, 0)
+	t := (s >> TIME_BITS_LOC) + DISCORD_EPOCH
+	sec := t / 1000.0
+	nano := t % 1000.0 * 1000000.0
+	creationTime := time.Unix(sec, nano)
 	workerID := (s & WORKER_ID_MASK) >> WORKER_ID_LOC
 	processID := (s & PROCESS_ID_MASK) >> PROCESS_ID_LOC
 	increment := s & INCREMENT_MASK
